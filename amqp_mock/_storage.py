9c2f3e5d139f31c78dd2ff30327d653113ee4233
E. Madison Bray <erik.bray@lri.fr>
2021-11-02 15:33:12 +0100

    add support for fanout exchanges
    topic/header exchanges can be left as a separate exercise
    
    depends on https://github.com/nikitanovosibirsk/amqp-mock/pull/8
    and https://github.com/nikitanovosibirsk/amqp-mock/pull/9

--- amqp_mock/_storage.py
+++ amqp_mock/_storage.py
@@ -8,36 +8,51 @@ from ._message import Message, MessageStatus, QueuedMessage
 class Storage:
     def __init__(self) -> None:
         self._exchanges: Dict[str, List[Message]] = {}
+        self._exchange_types: Dict[str, str] = {}
         self._queues: Dict[str, Queue[Message]] = {}
         self._history: Dict[str, QueuedMessage] = OrderedDict()
         self._binds: DefaultDict[str, Dict[str, str]] = defaultdict(dict)
 
     async def clear(self) -> None:
         self._exchanges = {}
+        self._exchange_types = {}
         self._queues = {}
         self._history = OrderedDict()
         self._binds = defaultdict(dict)
 
     async def add_message_to_exchange(self, exchange: str, message: Message) -> None:
-        if exchange not in self._exchanges:
-            self._exchanges[exchange] = []
+        await self.declare_exchange(exchange)
         self._exchanges[exchange].insert(0, message)
 
+        exchange_type = self._exchange_types[exchange]
         binds = self._binds.get(exchange)
-        routing_key = message.routing_key
 
-        if binds and routing_key in binds:
-            await self.add_message_to_queue(binds[routing_key], message)
+        if binds:
+            if exchange_type == "direct":
+                routing_key = message.routing_key
+
+                if routing_key in binds:
+                    await self.add_message_to_queue(binds[routing_key], message)
+            elif exchange_type == "fanout":
+                for queue in binds.values():
+                    await self.add_message_to_queue(queue, message)
+            else:
+                raise RuntimeError(f"{exchange_type} exchanges not supported")
 
     async def bind_queue_to_exchange(self, queue: str, exchange: str,
                                      routing_key: str = "") -> None:
+        await self.declare_queue(queue)
         self._binds[exchange][routing_key] = queue
+
+    async def declare_exchange(self, exchange: str, exchange_type: str = "direct") -> None:
+        if exchange not in self._exchanges:
+            self._exchanges[exchange] = []
+            self._exchange_types[exchange] = exchange_type
 
     async def declare_queue(self, queue: str) -> None:
         if queue not in self._queues:
             self._queues[queue] = Queue()
-
-        await self.bind_queue_to_exchange(queue, "", routing_key=queue)
+            await self.bind_queue_to_exchange(queue, "", routing_key=queue)
 
     async def get_messages_from_exchange(self, exchange: str) -> List[Message]:
         if exchange not in self._exchanges:
