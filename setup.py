7009207e45de557c7e733dc12811c079f5262f8e
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-11-07 11:22:04 +0300

    bump version → 0.3.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="amqp-mock",
-    version="0.2.0",
+    version="0.3.0",
     description="Remote AMQP mock",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
