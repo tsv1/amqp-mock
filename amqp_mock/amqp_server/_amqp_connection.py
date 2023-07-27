4374f44098cf72981afcb69bdd4df161feaa5dc7
Yuriy Sagitov <pro100.ko10ok@gmail.com>
2023-07-27 11:21:53 +0300

    fix unmarshal not waiting frame_end bug

--- amqp_mock/amqp_server/_amqp_connection.py
+++ amqp_mock/amqp_server/_amqp_connection.py
@@ -6,7 +6,9 @@ from typing import Any, AsyncGenerator, Awaitable, Callable, Dict, Optional, Tup
 
 from pamqp import base, commands
 from pamqp.body import ContentBody
+from pamqp.constants import FRAME_END
 from pamqp.exceptions import UnmarshalingException
+from pamqp.frame import FrameTypes
 from pamqp.frame import marshal, unmarshal
 from pamqp.header import ContentHeader, ProtocolHeader
 from pamqp.heartbeat import Heartbeat
@@ -99,6 +101,17 @@ class AmqpConnection:
         if self._on_close:
             await self._on_close(self)
 
+    async def _unmarshal(self, buffer: bytes) -> Tuple[int, int, FrameTypes]:
+        # Workaround for pamqp bug:
+        #   Heartbeat can be unmarshaled successfully even without FRAME_END.
+        #   this shouldn't happen according to spec
+
+        byte_count, channel_id, frame = unmarshal(buffer)
+        if frame.name == Heartbeat.name and buffer[-1] != FRAME_END:
+            raise UnmarshalingException()
+
+        return byte_count, channel_id, frame
+
     async def _reader_task(self, reader: StreamReader) -> None:
         buffer = b""
         while not reader.at_eof():
@@ -107,7 +120,7 @@ class AmqpConnection:
                 break
             buffer += chunk
             try:
-                byte_count, channel_id, frame = unmarshal(buffer)
+                byte_count, channel_id, frame = await self._unmarshal(buffer)
             except UnmarshalingException:
                 continue
             else:
