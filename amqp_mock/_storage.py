92a777d406b3469df7cc8ce4a0003764b0ffa537
moyiz <8603313+moyiz@users.noreply.github.com>
2023-11-08 10:55:17 +0200

    Change storage history from dict to list to support multiple copies of the same message with fanout exchanges

--- amqp_mock/_storage.py
+++ amqp_mock/_storage.py
@@ -1,6 +1,6 @@
 from asyncio import Queue
-from collections import OrderedDict, defaultdict
-from typing import AsyncGenerator, DefaultDict, Dict, List
+from collections import defaultdict
+from typing import AsyncGenerator, DefaultDict, Dict, List, Tuple
 
 from ._message import Message, MessageStatus, QueuedMessage
 
@@ -10,14 +10,14 @@ class Storage:
         self._exchanges: Dict[str, List[Message]] = {}
         self._exchange_types: Dict[str, str] = {}
         self._queues: Dict[str, Queue[Message]] = {}
-        self._history: Dict[str, QueuedMessage] = OrderedDict()
+        self._history: List[Tuple[str, QueuedMessage]] = []
         self._binds: DefaultDict[str, Dict[str, str]] = defaultdict(dict)
 
     async def clear(self) -> None:
         self._exchanges = {}
         self._exchange_types = {}
         self._queues = {}
-        self._history = OrderedDict()
+        self._history = []
         self._binds = defaultdict(dict)
 
     async def add_message_to_exchange(self, exchange: str, message: Message) -> None:
@@ -66,13 +66,15 @@ class Storage:
     async def add_message_to_queue(self, queue: str, message: Message) -> None:
         await self.declare_queue(queue)
         await self._queues[queue].put(message)
-        self._history[message.id] = QueuedMessage(message, queue)
+        self._history.append((message.id, QueuedMessage(message, queue)))
 
     async def get_history(self) -> List[QueuedMessage]:
-        return [message for message in self._history.values()][::-1]
+        return [message[1] for message in self._history[::-1]]
 
     async def change_message_status(self, message_id: str, status: MessageStatus) -> None:
-        self._history[message_id].set_status(status)
+        for msg_id, message in self._history:
+            if msg_id == message_id:
+                message.set_status(status)
 
     async def get_next_message(self, queue: str) -> AsyncGenerator[Message, None]:
         if queue not in self._queues:
